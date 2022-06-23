2d412c5878ce82a08867727756b090730b23718e
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-06-24 06:41:44 +0900

    RSpecインストール完了

---
+++ spec/system/task_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+RSpec.describe 'タスク管理機能', type: :system do
+  describe '新規作成機能' do
+    context 'タスクを新規作成した場合' do
+      it '作成したタスクが表示される' do
+        task = FactoryBot.create(:task, title: 'task')
+        visit task_path
+        expect(page).to have_content 'task'
+      end
+    end
+  end
+end
