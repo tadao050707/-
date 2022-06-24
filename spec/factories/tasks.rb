2d340e43c0a01f7da0ac291d8fb15138640804a3
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-06-24 14:13:38 +0900

    systemspec正常に動作

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :task do
-    # 下記の内容は実際に作成するカラム名に合わせて変更してください
-    title { 'test_title' }
-    content { 'test_content' }
+    name { 'task_name' }
+    content { 'task_content' }
   end
+
+  
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,11 +2,37 @@ require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
-      it '作成したタスクが表示される' do
-        task = FactoryBot.create(:task, title: 'task')
-        visit task_path
+      before do
+        visit new_task_path
+        fill_in 'task_name', with: 'task.name'
+        fill_in 'task_content', with: 'task.content'
+        click_button 'Create Task'
+      end
+
+      it '正常に登録される' do
         expect(page).to have_content 'task'
       end
     end
   end
+
+  describe '一覧表示' do
+    context '一覧画面にアクセスしたとき' do
+      before do
+        visit tasks_path
+      end
+
+      it '作成済みのタスクが表示されている' do
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context '任意のタスク詳細画面に遷移した場合' do
+      before do
+        visit tasks_path
+      end
+      it '該当タスクの内容が表示される' do
+    end
+  end
+end
 end
