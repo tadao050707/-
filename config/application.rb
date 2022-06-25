1993331f1ebf70c18b344f526fa78a2aa5a03dc1
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-06-25 10:56:03 +0900

    タイムゾーン完了

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module ElTraining2Wef2205
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     config.generators do |g|
       g.test_framework :rspec,
@@ -18,7 +20,7 @@ module ElTraining2Wef2205
       helper_specs: false,
       routing_specs: false,
       controller_specs: false,
-      request_specs: false
+      request_specs: false,
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
